7a87b79c5929af58787fd08587c7c34a0650b3e1
danilovalerio <danilo_vs@hotmail.com>
2025-01-28 12:51:41 -0300

    Initial commit

---
+++ src/test/java/com/danilo/projeto/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.projeto.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
