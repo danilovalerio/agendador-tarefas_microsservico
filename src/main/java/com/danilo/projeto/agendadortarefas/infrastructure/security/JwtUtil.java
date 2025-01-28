8a9e163253b9cf86eadd4e0210435e126dee4543
danilovalerio <danilo_vs@hotmail.com>
2025-01-28 13:57:35 -0300

    Importacao das libs do JWT Token

--- src/main/java/com/danilo/projeto/agendadortarefas/infrastructure/security/JwtUtil.java
+++ src/main/java/com/danilo/projeto/agendadortarefas/infrastructure/security/JwtUtil.java
@@ -2,7 +2,6 @@ package com.danilo.projeto.agendadortarefas.infrastructure.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
